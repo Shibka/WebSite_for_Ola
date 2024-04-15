8487d96022fa5201305824465f62da002fc28c35
Aliaksei <shebalinas2000@gmail.com>
2024-04-15 08:22:25 +0200

    feat: added english version of website

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-let a = "Мы постоянно совершенствуемся и ищем новые возможности для роста и развития!"
+let a = "WE ARE CONSTANTLY IMPROVING AND LOOKING FOR NEW OPPORTUNITIES FOR GROWTH AND DEVELOPMENT!"
 let b = a.toUpperCase()
 console.log(b)
+
+
+
+function switchLanguage() {
+    const currentLang = document.getElementById('lang-switch').textContent;
+    if (currentLang === 'EN') {
+        // Logic to switch to English
+        window.location.href = './indexEN.html'; // Adjust URL if using subdirectories
+        document.getElementById('lang-switch').textContent = 'RU';
+    } else {
+        // Logic to switch to Russian
+        window.location.href = './index.html'; // Adjust URL for default language
+        document.getElementById('lang-switch').textContent = 'EN';
+    }
+}
