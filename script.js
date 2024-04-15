111cd918e8349114cd783cf74fac015f23372038
Aliaksei <shebalinas2000@gmail.com>
2024-04-15 12:01:15 +0200

    feat: test

--- script.js
+++ script.js
@@ -6,17 +6,29 @@ console.log(b)
 
 function switchLanguage() {
     const langSwitchElement = document.getElementById('lang-switch');
+    const searchField = document.getElementById('searchField');
     const currentLang = langSwitchElement.textContent;
 
     if (currentLang === 'EN') {
         // Change to English version
-        window.location.href = 'EN.html';  // Assuming file naming
+        window.location.href = 'EN.html';
         langSwitchElement.textContent = 'RU';
-        localStorage.setItem('preferredLanguage', 'EN');  // Store preference
+        localStorage.setItem('preferredLanguage', 'EN');
+        localStorage.setItem('searchFieldValue', searchField.value);  // Save current search value
     } else {
         // Change to Russian version
-        window.location.href = 'index.html';
+        window.location.href = 'RU.html';
         langSwitchElement.textContent = 'EN';
-        localStorage.setItem('preferredLanguage', 'RU');  // Store preference
+        localStorage.setItem('preferredLanguage', 'RU');
+        localStorage.setItem('searchFieldValue', searchField.value);  // Save current search value
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchField = document.getElementById('searchField');
+    // Check if there is a stored value for the search field
+    const savedSearchValue = localStorage.getItem('searchFieldValue');
+    if (savedSearchValue) {
+        searchField.value = savedSearchValue;
+    }
+});
